653a3622cc7fb3b103ac600eac2362286075e7dd
Victor Bejarano <alejandrobjrn4@gmail.com>
2024-02-18 17:37:25 -0500

    7. Calculando el total con ngOnChanges

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,23 +1,20 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Product } from '../../../shared/models/product.model';
+import { Product } from 'src/app/domains/shared/models/product.model';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css'],
+  styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  @Input({ required: true }) product!: Product;
+  @Input({required: true}) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler() {
-    console.log('click form child');
-    this.addToCart.emit(
-      'hola este es un msg desde el hijo ' + this.product.title
-    );
+    this.addToCart.emit(this.product);
   }
 }
